fc36bcd714d54f436f155afe74ce0830d32411eb
M <markpackham1@gmail.com>
2024-12-11 13:34:59 +0000

    Demo append() and clear() for strings
    https://youtu.be/-TkoO8Z07hI?feature=shared&t=5601

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,18 +7,20 @@ int main()
     string name;
 
     cout << "Enter your name: ";
+    name.clear();
     getline(cin, name);
 
     if (name.length() > 100)
     {
         cout << "Your name can't be over 100 chars";
     }
-    else if (name.empty()){
+    else if (name.empty())
+    {
         cout << "You cannot have an empty name";
     }
     else
     {
-        cout << "Welcome " << name;
+        cout << "Welcome " << name.append("@gmail.com");
     }
 
     return 0;
