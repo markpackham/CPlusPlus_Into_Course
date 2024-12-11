0d083979fdc3fc3bce985e1369a12b46f502f178
M <markpackham1@gmail.com>
2024-12-11 10:10:33 +0000

    Demo explicit cast for ascii character of 100
    https://youtu.be/-TkoO8Z07hI?feature=shared&t=2728

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,19 +1,9 @@
 #include <iostream>
 #include <vector>
 
-// typedef std::string text_t;
-// typedef int number_t;
-
-using text_t = std::string;
-using number_t = int;
-
 int main()
 {
-    text_t firstName = "Bob";
-    number_t age = 27;
-
-    std::cout << firstName << '\n';
-    std::cout << age << '\n';
+    std::cout << (char)100;
 
     return 0;
 }
