b63b2082f2c5630185fd6384326b74d0cdf6c3b8
M <markpackham1@gmail.com>
2024-12-11 10:30:49 +0000

    Use std::ws to eliminate white spaces & new lines before user input
    https://youtu.be/-TkoO8Z07hI?feature=shared&t=3157

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,11 +6,11 @@ int main()
     std:: string name;
     int age;
 
-    std::cout << "What is your name?: ";
-    std::getline(std::cin, name);
-
     std::cout << "How old are you?: ";
     std::cin >> age;
+
+    std::cout << "What is your name?: ";
+    std::getline(std::cin >> std::ws, name);
 
     std::cout << "Hello " << name << "!" << std::endl;
     std::cout << "You are " << age << " years old." << std::endl;
