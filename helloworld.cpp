87d54e96d0a7f490f813fabcdd40f01aaf2d41a3
M <markpackham1@gmail.com>
2024-12-11 09:42:54 +0000

    Use "using" keyword over less popular "typedef" since it works better with templates
    https://youtu.be/-TkoO8Z07hI?feature=shared&t=2222

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
 
-typedef std::string text_t;
+// typedef std::string text_t;
+// typedef int number_t;
+
+using text_t = std::string;
+using number_t = int;
 
 int main()
 {
     text_t firstName = "Bob";
+    number_t age = 27;
 
     std::cout << firstName << '\n';
+    std::cout << age << '\n';
 
     return 0;
 }
