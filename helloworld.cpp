ba8a561ebb85d0889d44c01079f3ccada95cbd82
M <markpackham1@gmail.com>
2024-12-11 10:25:43 +0000

    Demo cin for character input
    https://youtu.be/-TkoO8Z07hI?feature=shared&t=3005

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,7 +3,17 @@
 
 int main()
 {
-    std::cout << (char)100;
+    std:: string name;
+    int age;
+
+    std::cout << "What is your name?: ";
+    std::cin >> name;
+
+    std::cout << "How old are you?: ";
+    std::cin >> age;
+
+    std::cout << "Hello " << name << "!" << std::endl;
+    std::cout << "You are " << age << " years old." << std::endl;
 
     return 0;
 }
