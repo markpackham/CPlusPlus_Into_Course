d42782f897c3e8eb4dbdc5b2200415d376510533
M <markpackham1@gmail.com>
2024-12-11 09:18:41 +0000

    Demo use of constant
    https://youtu.be/-TkoO8Z07hI?feature=shared&t=1593

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 int main() {
-    
-    int x = 5;
 
-    std::cout << x;
+   const double PI = 3.14159;
+   double radius = 10;
+   double circumference = 2 * PI * radius;
 
+   std::cout << circumference << "cm";  
     return 0;
 }
