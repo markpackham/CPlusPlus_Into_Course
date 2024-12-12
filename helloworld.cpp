dc9128cbe89cfbb20f41be9a820d6def52f616d0
M <markpackham1@gmail.com>
2024-12-12 17:24:45 +0000

    Demo size of strings
    https://youtu.be/-TkoO8Z07hI?si=-2trR0uK9gCh1eLL&t=10699

--- helloworld.cpp
+++ helloworld.cpp
@@ -14,8 +14,12 @@ void printNum();
 int main()
 {
 
+    string name1 = "John";
+    string name2 = "JohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohn";
     double gpa = 2.5;
 
+    cout << sizeof(name1) << " bytes" << endl;
+    cout << sizeof(name2) << " bytes" << endl;
     cout << sizeof(gpa) << " bytes" << endl;
 
     return 0;
