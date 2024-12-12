8dd949958ee22e9ae57a5a164ac1717a883f1ca1
M <markpackham1@gmail.com>
2024-12-12 17:41:31 +0000

    Demo sizeof() with string array & how to find the number of elements in it
    https://youtu.be/-TkoO8Z07hI?si=fVXDK3AJbssGEobr&t=10898

--- helloworld.cpp
+++ helloworld.cpp
@@ -18,9 +18,15 @@ int main()
     string name2 = "JohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohn";
     double gpa = 2.5;
 
+    char grades[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+    string names[] = {"Bob", "Jane", "Jim", "Sue"};
+
     cout << sizeof(name1) << " bytes" << endl;
     cout << sizeof(name2) << " bytes" << endl;
     cout << sizeof(gpa) << " bytes" << endl;
+    // How to find out the number of elements in an array of type char
+    cout << sizeof(grades)/sizeof(char) << " elements in the array of chars" << endl;
+    cout << sizeof(names)/sizeof(string) << " elements in the array of strings" << endl;
 
     return 0;
 }
