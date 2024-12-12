5727d42f7bff44c3d220876a91931fd8162229a1
M <markpackham1@gmail.com>
2024-12-12 16:24:42 +0000

    Demo another overloaded function
    https://youtu.be/-TkoO8Z07hI?si=31T6UURIMqu-N6Q3&t=8394

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 void bakePizza();
 void bakePizza(string topping1);
+void bakePizza(string topping1, string topping2);
 
 int main()
 {
 
     bakePizza();
-    bakePizza("ham");
+    bakePizza("bacon");
+    bakePizza("ham", "pineapple");
 
     return 0;
 }
@@ -23,4 +25,9 @@ void bakePizza()
 void bakePizza(string topping1)
 {
     cout << "Here is your " << topping1 << " pizza!\n";
+}
+
+void bakePizza(string topping1, string topping2)
+{
+    cout << "Here is your " << topping1 << " & " << topping2 << " pizza!\n";
 }
