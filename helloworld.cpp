e98e7cc02c7a3d7d8306a72ea56a00d486d4c3e1
M <markpackham1@gmail.com>
2024-12-14 09:48:37 +0000

    Demo for each --- loop for( : )
    https://youtu.be/-TkoO8Z07hI?feature=shared&t=11246

--- helloworld.cpp
+++ helloworld.cpp
@@ -14,19 +14,11 @@ void printNum();
 int main()
 {
 
-    string name1 = "John";
-    string name2 = "JohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohn";
-    double gpa = 2.5;
+ string students[] = {"Bob","John","Frank","Sandy"};
 
-    char grades[] = {'A', 'B', 'C', 'D', 'E', 'F'};
-    string names[] = {"Bob", "Jane", "Jim", "Sue"};
-
-    cout << sizeof(name1) << " bytes" << endl;
-    cout << sizeof(name2) << " bytes" << endl;
-    cout << sizeof(gpa) << " bytes" << endl;
-    // How to find out the number of elements in an array of type char
-    cout << sizeof(grades)/sizeof(char) << " elements in the array of chars" << endl;
-    cout << sizeof(names)/sizeof(string) << " elements in the array of strings" << endl;
-
+// For Each :
+for(string student : students){
+    cout << student << endl;
+}
     return 0;
 }
