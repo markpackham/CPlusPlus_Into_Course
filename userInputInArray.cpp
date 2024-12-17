fbd97e7c03d4c5473eab5457283e830ed0cadc5f
M <markpackham1@gmail.com>
2024-12-17 17:22:32 +0000

    Make sure "q" to quit isn't mistaken for a type of food
    https://youtu.be/-TkoO8Z07hI?si=XNKh84krTdX-4Ong&t=13004

--- userInputInArray.cpp
+++ userInputInArray.cpp
@@ -6,19 +6,28 @@ int main()
     // Array that user will populate later
     string foods[5];
     int size = sizeof(foods) / sizeof(foods[0]);
+    // To hold user input so "q" for quit not mistaken as food
+    string temp;
 
     for (int i = 0; i < size; i++)
     {
-        cout << "Enter a food you like or 'q' to quit #" << i+1 << ": ";
-        getline(cin, foods[i]);
-        if(foods[i] == "q"){
+        cout << "Enter a food you like or 'q' to quit #" << i + 1 << ": ";
+        getline(cin, temp);
+        // Make sure the "q" to quit isn't mistaken for a food
+        if (temp == "q")
+        {
             break;
+        }
+        else
+        {
+            foods[i] = temp;
         }
     }
 
     cout << "Your favorite foods are: \n\n";
 
-    for(string food : foods){
+    for (string food : foods)
+    {
         cout << food << endl;
     }
 
