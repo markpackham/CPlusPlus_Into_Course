ccb1eecc9aa81de5d0cb307ac542e52be17c5d18
M <markpackham1@gmail.com>
2024-12-17 17:45:24 +0000

    Add 2D array
    https://youtu.be/-TkoO8Z07hI?si=cSgLQtsaZWwZ1kAw&t=13237

---
+++ multiDimensionalArrays.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string cars[][3] = {{"Mustang", "Escape", "F-150"}, {"Corvette", "Equinox", "Silverado"}, {"Challenger", "Durango", "Ram 1500"}};
+
+    return 0;
+}
