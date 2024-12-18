444879d33cadec068a37618970d7c1d4c1dfcaea
M <markpackham1@gmail.com>
2024-12-18 13:48:12 +0000

    Print out 2D Array
    https://youtu.be/-TkoO8Z07hI?feature=shared&t=13560

--- multiDimensionalArrays.cpp
+++ multiDimensionalArrays.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int main()
 {
-    string cars[][3] = {{"Mustang", "Escape", "F-150"}, {"Corvette", "Equinox", "Silverado"}, {"Challenger", "Durango", "Ram 1500"}};
+    string cars[][3] = {{"Mustang", "Escape", "F-150"}, {"Corvette", "Equinox", "Silverado"}, {"Challenger", "Durango", "Ram 1500"}, {"Batmobile", "Popemobile", "Automobile"}};
+
+    int rows = sizeof(cars) / sizeof(cars[0]);
+    int cols = sizeof(cars[0]) / sizeof(cars[0][0]);
+
+    // Outer loop deals with rows
+    for (int i = 0; i < rows; i++)
+    {
+        // Inner loop deals with columns
+        for (int j = 0; j < cols; j++)
+        {
+            cout << cars[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
