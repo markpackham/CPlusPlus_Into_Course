814e79ed548c325f6bf38d7d7c2ddebe25121ff8
M <markpackham1@gmail.com>
2024-12-18 22:26:46 +0000

    Demo pointer with array
    https://youtu.be/yKT_Rwx4lsQ?feature=shared&t=2

--- pointers.cpp
+++ pointers.cpp
@@ -11,10 +11,20 @@ int main()
     // * dereference operator
 
     string name = "Bob";
+    int age = 20;
+    string freePizzas[5] = {"pizza1", "pizza2", "pizza3", "pizza4", "pizza5"};
+
     string *namePtr = &name;
+    int *agePtr = &age;
+    // Since the array is already an address we don't need to use &
+    // so don't do &freePizzas
+    string *freePizzasPtr = freePizzas;    
 
     // Show "Bob" when pointer is dereferenced
-    cout << *namePtr;
+    cout << *namePtr << endl;
+    cout << *agePtr << endl;
+    // Gives first element in array
+    cout << *freePizzasPtr << endl;
 
     return 0;
 }
