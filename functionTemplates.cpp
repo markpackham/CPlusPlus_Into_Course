fd074a824d71b96e62e77b5d780b2a9800962728
M <markpackham1@gmail.com>
2024-12-20 18:29:58 +0000

    Add notes for function templates
    https://youtu.be/spZd2rNtze8?feature=shared&t=2

---
+++ functionTemplates.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Function templates = describe what a function looks like
+    // can be used to generate as many overloaded functions
+    // as needed each using different data types / Generics
+
+    return 0;
+}
--- recursionVsIteration.cpp
+++ recursionVsIteration.cpp
@@ -5,6 +5,9 @@ using namespace std;
 void walkIterative(int steps);
 void walkRecursive(int steps);
 
+int factorialIterative(int num);
+int factorialRecursive(int num);
+
 int main()
 {
     // Recursion, a technique where a function invokes itself from within
@@ -14,11 +17,21 @@ int main()
     // Recursion can lead to cleaner & less code, great for sorting & search algorithms
     // However recursive code uses more memory & can be slower/processing time as well as harder to debug
 
-    cout << "Iterative" << endl;
+    cout << "Iterative Walk" << endl;
     walkIterative(5);
     cout << endl;
-    cout << "Recursive" << endl;
+    cout << "Recursive Walk" << endl;
     walkRecursive(5);
+
+    cout << endl;
+    cout << "Iterative Factoral" << endl;
+    // Returns 120
+    cout << factorialIterative(5);
+    cout << endl;
+    cout << "Recursive Factoral" << endl;
+    // Returns 120
+    cout << factorialRecursive(5);
+    cout << endl;
 
     return 0;
 }
@@ -37,5 +50,27 @@ void walkRecursive(int steps)
     {
         cout << "You take a step!\n";
         walkRecursive(steps - 1);
+    }
+}
+
+int factorialIterative(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+int factorialRecursive(int num)
+{
+    if (num > 1)
+    {
+        return num * factorialRecursive(num - 1);
+    }
+    else
+    {
+        return 1;
     }
 }
