2a4fac3b1d3026f91a144280e1249f3011d6649a
M <markpackham1@gmail.com>
2024-12-22 12:27:15 +0000

    Demo constructor

--- constructors.cpp
+++ constructors.cpp
@@ -4,10 +4,32 @@ using namespace std;
 
 class Car
 {
+public:
+    string make;
+    string model;
+    int year;
+    string color;
+
+    // Constructor (like most other langues it has the same name as the class)
+    Car(string make, string model, int year, string color)
+    {
+        this->make = make;
+        this->model = model;
+        this->year = year;
+        this->color = color;
+    }
 };
 
 int main()
 {
+
+    Car car1("Chevy", "Corvette", 2022, "blue");
+    Car car2("Ford", "Mustang", 2023, "red");
+
+    cout << car1.make << '\n';
+    cout << car1.model << '\n';
+    cout << car1.year << '\n';
+    cout << car1.color << '\n';
 
     return 0;
 }
